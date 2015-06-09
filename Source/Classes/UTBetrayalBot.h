e7dd55fcef4c82535d89f966a6b245a6a6125263
RattleSN4K3 <RattleSN4K3@gmail.com>
2015-06-09 23:19:30 +0200

    Cleanup bot team check in betrayal-mode

--- Source/Classes/UTBetrayalBot.h
+++ Source/Classes/UTBetrayalBot.h
@@ -9,21 +9,11 @@ class AUTBetrayalBot : public AUTBot
 {
 	GENERATED_UCLASS_BODY()
 
-private:
-
-	bool bPickProcessing;
-
-public:
-
 	/** whether this bot should try to betray a team member */
 	UPROPERTY()
 	bool bBetrayTeam;
 
-	//virtual void UpdateEnemyInfo(APawn* NewEnemy, EAIEnemyUpdateType UpdateType) override;
-	virtual bool IsImportantEnemyUpdate(APawn* TestEnemy, EAIEnemyUpdateType UpdateType) override;
-	virtual void PickNewEnemy();
-
-	UFUNCTION()
-	virtual bool GetPawnByPRI(AUTPlayerState* PRI, APawn*& P);
+	/** return whether passed in Actor is or belongs to a teammate */
+	virtual bool IsTeammate(AActor* TestActor) override;
 
 };
--- Source/Private/UTBetrayalBot.cpp
+++ Source/Private/UTBetrayalBot.cpp
@@ -5,80 +5,20 @@
 #include "UTBetrayalTeam.h"
 #include "UTBot.h"
 
+// TODO: rate enemy if bBetrayteam and teammate
+
 AUTBetrayalBot::AUTBetrayalBot(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 }
 
-void AUTBetrayalBot::PickNewEnemy()
+bool AUTBetrayalBot::IsTeammate(AActor* TestActor)
 {
-	if (bBetrayTeam && GetPawn() != NULL)
-	{
-		if (AUTBetrayalGameState* GS = GetWorld()->GetGameState<AUTBetrayalGameState>())
-		{
-			AUTBetrayalPlayerState* BPRI = Cast<AUTBetrayalPlayerState>(PlayerState);
-			if (BPRI != NULL && BPRI->CurrentTeam != NULL)
-			{
-				// as we call UpdateEnemyInfo currently, we need to skip the team check
-				GS->bSkipCheck = true;
-				bPickProcessing = true;
-
-				for (int32 i = 0; i < ARRAY_COUNT(BPRI->CurrentTeam->Teammates); i++)
-				{
-					APawn* Enemy = NULL;
-					if (BPRI != BPRI->CurrentTeam->Teammates[i] && GetPawnByPRI(BPRI->CurrentTeam->Teammates[i], Enemy))
-					{
-						// condtionally add team member to enemy list
-						UpdateEnemyInfo(Enemy, EUT_Seen);
-					}
-				}
-
-				// revert
-				GS->bSkipCheck = false;
-				bPickProcessing = false;
-			}
-		}
-	}
-
-	// Pick enemy based on the current (and possibly modified) list of enemies
-	Super::PickNewEnemy();
-}
-
-bool AUTBetrayalBot::IsImportantEnemyUpdate(APawn* TestEnemy, EAIEnemyUpdateType UpdateType)
-{
-	// prevent recursion in UpdateEnemyInfo query for importance and calling PickNewEnemy again
-	if (bPickProcessing)
+	// shoot at everyone if on betray-mode
+	if (bBetrayTeam)
 	{
 		return false;
 	}
 
-	bool bIsImportant = Super::IsImportantEnemyUpdate(TestEnemy, UpdateType);
-
-	if (bBetrayTeam && !bIsImportant && TestEnemy != NULL)
-	{
-		if (AUTBetrayalGameState* GS = GetWorld()->GetGameState<AUTBetrayalGameState>())
-		{
-			if (GS->OnSameTeam(this, TestEnemy))
-			{
-				bIsImportant = true;
-			}
-		}
-	}
-
-	return bIsImportant;
-}
-
-bool AUTBetrayalBot::GetPawnByPRI(AUTPlayerState* PRI, APawn*& P)
-{
-	P = NULL;
-	if (PRI != NULL)
-	{
-		if (AController* C = Cast<AController>(PRI->GetOwner()))
-		{
-			P = C->GetPawn();
-			return P != NULL;
-		}
-	}
-
-	return false;
+	return Super::IsTeammate(TestActor);
 }
